e084618f989c44ba3e7348a11c54c0e25f44f89f
Mohankumar <vmohankumar@paypalcorp.com>
2016-09-28 20:18:15 +0530

    Login Controller added

---
+++ controllers/login.js
@@ -0,0 +1,22 @@
+var app = angular.module('Sensem', ["ngRoute"]);
+app.config(function ($routeProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'index.html'
+        })
+        .when('/dashboard', {
+            templateUrl: 'dashboard.html'
+        });
+});
+
+app.controller("loginController",function($scope,$location){
+ $scope.submit=function(){
+     var username=$scope.username;
+     var password=$scope.password;
+     if($scope.username=="admin" && $scope.password=="admin"){
+         $location.path('/dashboard');
+     }
+ };
+
+});
